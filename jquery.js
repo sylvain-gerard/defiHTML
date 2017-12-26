06977b9a14501636f28b974d0147744572bf1a60
Gerard <sylvaingerard4@hotmail.com>
2017-12-26 23:03:04 +0100

    reoutches visuelles + datepicker marche

--- jquery.js
+++ jquery.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $('.carousel').carousel();
     $('select').material_select();
-    Materialize.updateTextFields();
+    $('.datepicker').pickadate({
+        format: 'yyyy/mm/dd',
+    });
 });
